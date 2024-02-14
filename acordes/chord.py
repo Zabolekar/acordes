c7c9a5c40153285882b9a759c1310b63ef35f43e
Arsenii A <arseniiv@users.noreply.github.com>
2024-02-14 21:45:24 +0500

    Add more chord types
    taken from https://en.wikipedia.org/wiki/Chord_notation (most part)

--- acordes/chord.py
+++ acordes/chord.py
@@ -15,7 +15,31 @@ suffix_meanings = {
     "m7":     (P1, m3, P5, m7),
     "mM7":    (P1, m3, P5, M7),
     "7":      (P1, M3, P5, m7),
-    "M7":     (P1, M3, P5, M7)
+    "M7":     (P1, M3, P5, M7),
+    "aug":    (P1, M3, m6),
+    "dim":    (P1, m3, A4),
+    "dim7":   (P1, m3, A4, M6),
+    "m7b5":   (P1, m3, A4, m7),
+    "7b5":    (P1, M3, A4, m7),
+    "aug7":   (P1, M3, m6, m7),
+    "augM7":  (P1, M3, m6, M7),
+    "sus2":   (P1, M2, P5),
+    "sus4":   (P1, P4, P5),
+    "9":      (P1, M3, P5, m7, M2),
+    "m9":     (P1, m3, P5, m7, M2),
+    "M9":     (P1, M3, P5, M7, M2),
+    "mM9":    (P1, m3, P5, M7, M2),
+    "7b9":    (P1, M3, P5, m7, m2),
+    "aug9":   (P1, M3, m6, m7, M2),
+    "augM9":  (P1, M3, m6, M7, M2),
+    "dim9":   (P1, m3, A4, M6, M2),
+    "dimb9":  (P1, m3, A4, M6, m2),
+    "m9b5":   (P1, m3, A4, m7, M2),
+    "mb9b5":  (P1, m3, A4, m7, m2),
+    "2":      (P1, M3, P5, M2),      # aka add9
+    "m2":     (P1, m3, P5, M2),      # aka madd9
+    "6":      (P1, M3, P5, M6),      # aka add13
+    "m6":     (P1, m3, P5, M2)       # aka madd13
 }
 
 
