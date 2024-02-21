5499845db41cc5f76f684c836f247223a3abff8f
formicant <formicant@gmail.com>
2024-02-21 18:01:09 +0700

    Reexport from inner modules

--- acordes/__init__.py
+++ acordes/__init__.py
@@ -1,1 +1,9 @@
 from .tuning import Tuning
+from .fret import Fret
+from .formatting import (
+    FormattingOptions,
+    default_formatting_options,
+    compatibility_formatting_options,
+    OctaveNumbers,
+    Colorization
+)
