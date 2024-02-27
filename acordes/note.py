1939da64c7691665843beebd68532a9bf8200219
Zabolekar <syekirin@gmail.com>
2024-02-27 12:05:01 +0100

    better error messages for Cb, E# etc.

--- acordes/note.py
+++ acordes/note.py
@@ -12,12 +12,14 @@ _note_names = [
     ['B'],
 ]
 
+# our code is key-agnostic, so supporting notation like Cb wouldn't be useful anyway
+_unsupported = { 'Cb' : 'B', 'B#' : 'C', 'Fb' : 'E', 'E#' : 'F' }
+
 _note_index_by_name = {name: i for i, names in enumerate(_note_names) for name in names}
 
+note_name_regex = r"[A-G][#b]?"  # used by chord.py
 
-note_name_regex = r"[A-G][#b]?"
-
-note_regex = re.compile(fr"({note_name_regex})(\d)?")  # note name, then octave number
+_note_regex = re.compile(fr"({note_name_regex})(\d)?")  # note name, then octave number
 
 
 class Note:
@@ -27,8 +29,11 @@ class Note:
     `octave` is the octave number (from 0 to 9) for an absolute note or `None` for an octave-invariant note.
     """
     def __init__(self, name: str):
-        if match := note_regex.fullmatch(name):
+        if match := _note_regex.fullmatch(name):
             note_name, octave_name = match.groups()
+            if note_name in _unsupported:
+                suggestion = _unsupported[note_name]
+                raise ValueError(f"use {suggestion} instead of {note_name}")
             self.pitch_class = _note_index_by_name[note_name]
             self.octave = int(octave_name) if octave_name is not None else None
         else:
