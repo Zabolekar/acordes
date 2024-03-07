dad9c8a54ac1df38eb9b6ce4996e973ff0251e28
Zabolekar <syekirin@gmail.com>
2024-03-07 18:03:01 +0100

    moved formatting-related code from tuning to formatting

--- src/acordes/formatting.py
+++ src/acordes/formatting.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+from typing import Iterator
 from .chord import Chord
 from .note import Note
 
@@ -75,16 +75,28 @@ def _format_row(row: list[Note|None], root_note: Note) -> str:
     return ''.join(_format_fret(note, root_note) for note in row)
 
 
-def print_fretboard(chord: Chord, fretboard: list[list[Note|None]]) -> None:
-    """
-    fretboard
-        Should be non-empty, with all elements having the same length.
-    """
-    column_count = len(fretboard[0])
+def _fretted_strings(open_strings: list[Note], allowed_pitches: set[int], column_count: int) -> Iterator[list[Note|None]]:
+    for open_string in open_strings:
+        fretted_string: list[Note|None] = []
+        for fret in range(column_count):
+            note = open_string + fret
+            if note.pitch_class in allowed_pitches:
+                fretted_string.append(note)
+            else:
+                fretted_string.append(None)
+        yield fretted_string
+
+
+def print_fretboard(open_strings: list[Note], chord_name: str, fret_count: int) -> None:
+    chord = Chord(chord_name)
+    allowed_pitches = set(n.pitch_class for n in chord.notes)
+    column_count = fret_count + 1
+    fretboard = list(_fretted_strings(open_strings, allowed_pitches, column_count))
+
     width = column_count * 4
     print(f' {chord} '.center(width, '-'))
     print(''.join(f'{i:<4}' for i in range(column_count)))  # fret numbers
     root_note = chord.notes[0]
-    for row in reversed(fretboard):
+    for row in reversed(fretboard):  # each row corresponds to one string
         print(_format_row(row, root_note))
     print('-' * width)
--- src/acordes/tuning.py
+++ src/acordes/tuning.py
@@ -1,6 +1,5 @@
 from typing import Iterator
 import re
-from .chord import Chord
 from .note import Note
 from .formatting import print_fretboard
 
@@ -25,22 +24,8 @@ class Tuning:
     def __repr__(self) -> str:
         return self._repr
 
-    def _fretted_strings(self, allowed_pitches: set[int], fret_count: int) -> Iterator[list[Note|None]]:
-        for open_string in self.open_strings:
-            fretted_string: list[Note|None] = []
-            for fret in range(fret_count + 1):
-                note = open_string + fret
-                if note.pitch_class in allowed_pitches:
-                    fretted_string.append(note)
-                else:
-                    fretted_string.append(None)
-            yield fretted_string
-
     def __call__(self, chord_name: str, fret_count: int=12) -> None:
         """
         Print the fret diagram.
         """
-        chord = Chord(chord_name)
-        allowed_pitches = set(n.pitch_class for n in chord.notes)
-        fretted_strings = list(self._fretted_strings(allowed_pitches, fret_count))
-        print_fretboard(chord, fretted_strings)
+        print_fretboard(self.open_strings, chord_name, fret_count)
